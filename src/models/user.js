1420817f81ef18e4649108a92adf794432683972
HamedKh99 <goo.hamedacc@gmail.com>
2021-04-16 22:08:23 +0430

    add token generating to user

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema(
   {
@@ -67,6 +68,16 @@ userSchema.methods.toJSON = function () {
     delete userObject.tokens
 
     return userObject
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
